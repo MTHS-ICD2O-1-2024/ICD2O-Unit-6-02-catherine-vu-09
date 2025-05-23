dcd6cfe45b36aa7b9de0f13973eddefeda03b131
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-05-23 17:19:36 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine Vu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine Vu
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function counts the number of clicking of cookie
+ */
+// eslint-disable-next-line no-unused-vars
+function cookieClicker() {
+  // input
+  let cookieClick = localStorage.getItem('cookieClick')
+
+  // process
+  if (cookieClick != null) {
+    cookieClick = Number(cookieClick) + 1
+  } else {
+    cookieClick = 1
+  }
+
+  localStorage.setItem('cookieClick', cookieClick)
+
+  // output
+  document.getElementById('answer').innerHTML = cookieClick + ' Cookies'
+}
